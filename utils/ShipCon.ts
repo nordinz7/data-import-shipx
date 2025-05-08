f7752da65da0c33322a5e04c921a1c1b607869bf
Nordin <nordin.zahari@swiftlogistics.com.my>
2025-05-08 17:10:10 +0800

    fix: flatten company addresses

--- utils/ShipCon.ts
+++ utils/ShipCon.ts
@@ -1,88 +1,124 @@
-import { groupBy } from "lodash";
+import { groupBy, mapKeys } from "lodash";
 import { Opts, ShipperConDelivery, ShipperConsignee } from "../types";
 import { DEFAULT_IF_REQUIRED_NOT_FOUND } from "../constants";
-import { cloneJsonDataFromSheet, extractPostcode, getCityFromPostcode, getFullAddress, getLatLong, getStateFromPostcode } from "./xlsxToCsv";
+import {
+  cloneJsonDataFromSheet,
+  extractPostcode,
+  getCityFromPostcode,
+  getFullAddress,
+  getLatLong,
+  getStateFromPostcode,
+} from "./xlsxToCsv";
 
-export const convertToShipCon = (shipConCompanies: ShipperConsignee[], shipConAddresses: ShipperConDelivery[], opts: Opts) => {
-  const groupedAddresses = groupBy(shipConAddresses, 'ShipConCode');
-  const groupedAreaCodes = groupBy(cloneJsonDataFromSheet(opts.workbook.Sheets['AdmArea']), 'AreaCode');
+export const convertToShipCon = (
+  shipConCompanies: ShipperConsignee[],
+  shipConAddresses: ShipperConDelivery[],
+  opts: Opts
+) => {
+  const groupedAddresses = groupBy(shipConAddresses, "ShipConCode");
+  const groupedAreaCodes = groupBy(
+    cloneJsonDataFromSheet(opts.workbook.Sheets["AdmArea"]),
+    "AreaCode"
+  );
 
-  return shipConCompanies.map((company) => {
+  return shipConCompanies.flatMap((company) => {
     const foundAddresses = groupedAddresses[company.Code] || [];
-    const addresses = foundAddresses.map((address) => {
-      const fulladdr = getFullAddress(address, ['Address1', 'Address2', 'Address3', 'Address4']);
-      const postcode = extractPostcode(fulladdr);
-      const city = getCityFromPostcode(postcode!);
-      const state = getStateFromPostcode(postcode!);
 
-            return {
-            // --- Address ---
-            "name": address['Name']|| DEFAULT_IF_REQUIRED_NOT_FOUND,
-            "type": ['DELIVERY'],
-            "countryAlpha3": "MYS",
-            "address1": address["Address1"] || "",
-            "address2": address["Address2"] || "",
-            "address3": address["Address3"] || "",
-            "address4": address["Address4"] || "",
-            "city": city || address["City"] || "",
-            "state": state || address["State"] || "",
-            "district": city || address["City"] || "",
-            "postCode": postcode || "",
-            "areaCode": address["AreaCode"] || DEFAULT_IF_REQUIRED_NOT_FOUND,
-            "zone": groupedAreaCodes[address['AreaCode']]?.[0]?.AreaZone || DEFAULT_IF_REQUIRED_NOT_FOUND,
-            ...getLatLong(opts.buCode, company.Code),
-            "phone": address["Tel"] || "",
-            "fax": address["Fax"] || "",
-            "tags": parseInt(address['IsDefault']) ? ["isDefault"]: [],
-            "status": "activated",
-            "uuid": "",
-            "zzz": "",
-          }
-    }).filter(address => address.address1)
+    const addresses = foundAddresses
+      .map((address) => {
+        const fulladdr = getFullAddress(address, [
+          "Address1",
+          "Address2",
+          "Address3",
+          "Address4",
+        ]);
+        const postcode = extractPostcode(fulladdr);
+        const city = getCityFromPostcode(postcode!);
+        const state = getStateFromPostcode(postcode!);
+
+        return {
+          // --- Address ---
+          name: address["Name"] || DEFAULT_IF_REQUIRED_NOT_FOUND,
+          type: ["DELIVERY"],
+          countryAlpha3: "MYS",
+          address1: address["Address1"] || "",
+          address2: address["Address2"] || "",
+          address3: address["Address3"] || "",
+          address4: address["Address4"] || "",
+          city: city || address["City"] || "",
+          state: state || address["State"] || "",
+          district: city || address["City"] || "",
+          postCode: postcode || "",
+          areaCode: address["AreaCode"] || DEFAULT_IF_REQUIRED_NOT_FOUND,
+          zone:
+            groupedAreaCodes[address["AreaCode"]]?.[0]?.AreaZone ||
+            DEFAULT_IF_REQUIRED_NOT_FOUND,
+          ...getLatLong(opts.buCode, company.Code),
+          phone: address["Tel"] || "",
+          fax: address["Fax"] || "",
+          tags: parseInt(address["IsDefault"]) ? ["isDefault"] : [],
+          status: "activated",
+          uuid: "",
+          zzz: "",
+        };
+      })
+      .filter((address) => address.address1);
 
     const contacts = foundAddresses.map((address) => ({
-            // --- Contact ---
-            "name": address["Contact"] || "",
-            "email": "",
-            "phone": "",
-            "title": "",
-            "designation": "",
-            "notes": "",
-            "status": "activated",
-            "uuid": "",
-            "zzz": "",
-    }))
+      // --- Contact ---
+      name: address["Contact"] || "",
+      email: "",
+      phone: "",
+      title: "",
+      designation: "",
+      notes: "",
+      status: "activated",
+      uuid: "",
+      zzz: "",
+    }));
 
-    return {
-          // --- General Info ---
-          "no": "",
-          "code": company['Code'],
-          "name": company['Name'] || DEFAULT_IF_REQUIRED_NOT_FOUND,
-          "description": "",
-          "status": "activated",
-          "tags": "",
-          "overrideDuplicateCode": "TRUE",
-          "types": ['shipperConsignee'],
-          // --- Country & Currency ---
-          "country.name": "Malaysia",
-          "country.alpha3": "MYS",
-          "currency.code": "MYR",
-          "currency.uuid": "",
-          // --- Billing/Creditor ---
-          "billTo.code": "",
-          "billTo.uuid": "",
-          "creditorCode": "",
-          "creditorTerm": "",
-          // --- Debtor ---
-          "debtorCode": "",
-          "debtorTerm": "",
-          // --- Tax/Registration ---
-          "taxNumber": "",
-          "registration": "",
-          // --- UUID ---
-          "uuid": "",
-          addresses,
-          contacts
+    const baseObjCompany = {
+      // --- General Info ---
+      no: "",
+      code: company["Code"],
+      name: company["Name"] || DEFAULT_IF_REQUIRED_NOT_FOUND,
+      description: "",
+      status: "activated",
+      tags: "",
+      overrideDuplicateCode: "TRUE",
+      types: ["shipperConsignee"],
+      // --- Country & Currency ---
+      "country.name": "Malaysia",
+      "country.alpha3": "MYS",
+      "currency.code": "MYR",
+      "currency.uuid": "",
+      // --- Billing/Creditor ---
+      "billTo.code": "",
+      "billTo.uuid": "",
+      creditorCode: "",
+      creditorTerm: "",
+      // --- Debtor ---
+      debtorCode: "",
+      debtorTerm: "",
+      // --- Tax/Registration ---
+      taxNumber: "",
+      registration: "",
+      // --- UUID ---
+      uuid: "",
     };
+
+    if (!addresses.length) {
+      return [baseObjCompany];
+    }
+
+    return addresses.map((address, i) => {
+      const contact = contacts[i] || {};
+
+      return {
+        ...baseObjCompany,
+        ...mapKeys(address, (v, k) => `address.${k}`),
+        ...mapKeys(contact, (v, k) => `contact.${k}`),
+      };
+    });
   });
-}
+};
